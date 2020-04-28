34305afd779960ee53354ea7cd820fbf5d75636a
dad <60132399+dad2vad@users.noreply.github.com>
2020-04-28 18:09:52 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-x
+
 
 const production = !process.env.ROLLUP_WATCH;
 
